bdf1d3aea532bf48b0f37ac438d761a1b4090f3b
Hagezawa <hagezawavita@gmail.com>
2018-03-12 00:10:07 +0900

    ウィンドウの作成と背景の表示

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import tkinter
 
 #ウィンドウの作成
 root = tkinter.Tk()
+root.title("テストプログラム")
+root.minsize(640, 480)
+
+#画像表示
+#背景の表示と表示する背景の大きさの指定
+canvas = tkinter.Canvas(bg="black", width=640, height=480)
+
+#背景の位置の指定
+canvas.place(x=0, y=0)
 
 #メインループ
 root.mainloop()
