11265627070b69848084ff905c0c2bb3edf1db8a
Hagezawa <hagezawavita@gmail.com>
2018-03-12 20:14:24 +0900

    画像の表示と画像の位置の調整

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ canvas = tkinter.Canvas(bg="black", width=640, height=480)
 #背景の位置の指定
 canvas.place(x=0, y=0)
 
+img = tkinter.PhotoImage(file="img3/chap3-back.png")
+canvas.create_image(320, 240, image=img)
+
 #メインループ
 root.mainloop()
