2e6557c7484d61c115890ef60664e39ccf8aad3b
Hagezawa <hagezawavita@gmail.com>
2018-03-22 20:23:31 +0900

    テキストボックス、ボタン、テキストの設置
    Entry(テキストボックス),,,テキストボックスの大きさの指定
    Button(ボタン)...テキストの設定
    Label(ラベル)...テキストの設定と背景の色の設定
    place...位置の設定

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter
 root = tkinter.Tk()
 root.title("テストプログラム")
 root.minsize(640, 480)
+root.option_add("*font", ["MS Pゴシック", 22])
 
 #画像表示
 #背景の表示と表示する背景の大きさの指定
@@ -14,6 +15,22 @@ canvas.place(x=0, y=0)
 
 img = tkinter.PhotoImage(file="img3/chap3-back.png")
 canvas.create_image(320, 240, image=img)
+
+#テキストボックスを表示
+entry = tkinter.Entry(width=12, bd=4)
+entry.place(x=50, y=133)
+
+#質問ボタンを表示
+askbutton = tkinter.Button(text="聞く")
+askbutton.place(x=260, y=125)
+
+#テキストを表示
+question = tkinter.Label(text="知りたいのは何分かな?", bg="white")
+question.place(x=100, y=40)
+
+#答えを表示
+answer = tkinter.Label(text="...", bg="white")
+answer.place(x=115, y=235)
 
 #メインループ
 root.mainloop()
